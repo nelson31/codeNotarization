b4144b0f6f71953d4cb45e550ade1b81fa834e4b
Nelson Faria <a84727@uminho.pt>
2021-05-26 10:18:34 +0100

    alteracoes navbar

--- codeNotarization/ClientApp/src/components/NavBarOut.js
+++ codeNotarization/ClientApp/src/components/NavBarOut.js
@@ -34,8 +34,8 @@ export class NavBarOut extends Component {
                                 <a
                                     className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
                                 >
-                                    <img className="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-1 py-1 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
-                                        width={160} src={logo} />
+                                    <img className="text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-2 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                                        width={200} src={logo} />
                                 </a>
                             </Link>
                         </div>
@@ -47,22 +47,9 @@ export class NavBarOut extends Component {
                             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
 
                                 <li className="flex items-center">
-                                    <Link tag={Link} className="links" to="/registar">
-                                        <button
-                                            className="bg-white text-gray-800 active:bg-gray-100 text-s font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
-
-                                            type="button"
-                                            style={{ transition: "all .15s ease" }}
-                                        >
-                                            <i className="fas fa-arrow-alt-circle-down"></i> Registar
-                </button>
-                                    </Link>
-                                </li>
-
-                                <li className="flex items-center">
                                     <Link tag={Link} className="links" to="/login">
                                         <button
-                                            className="bg-white text-gray-800 active:bg-gray-100 text-s font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                                            className="bg-white text-gray-800 active:bg-gray-100 text-s font-bold uppercase px-8 py-3 rounded shadow hover:shadow-md outline-none focus:outline-none mr-8"
 
                                             type="button"
                                             style={{ transition: "all .15s ease" }}
